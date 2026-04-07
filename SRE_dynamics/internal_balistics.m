306f908faca845f98f1b1b7959f92fcebf0416f1
leoperina <leonardoperina2003@gmail.com>
2026-04-07 23:13:08 +0200

    Generation and validation of the Engine Simulink model
    generation of the Engine model, along with its validation, which consisted in confronting it with the MATLAB integrator

--- SRE_dynamics/internal_balistics.m
+++ SRE_dynamics/internal_balistics.m
@@ -1,4 +1,4 @@
-function [f] = internal_balistics(~, y, DATA)
+function [f] = internal_balistics(~, y, ENG, ENGfnc)
 %INTERNAL_BALISTICS function determining the internal balistics of the
 % rocket engine. 
 % The elements of the state space vector are: chamber pressure, grain
@@ -9,23 +9,23 @@ Pc = y(1);
 x  = y(2);                                                                  % m
 
 %_______________________________________________________________ grain data
-if x < DATA.xfin
-    Ab = DATA.Ab(x);
-    rb = DATA.a * Pc.^DATA.n;
-    Vc = DATA.Vc(x, DATA.L);
+if x < ENG.xfin
+    Ab = ENGfnc.Ab(x);
+    rb = ENG.a * Pc.^ENG.n;
+    Vc = ENGfnc.Vc(x, ENG.L);
 else
     Ab = 0;
     rb = 0;
-    Vc = DATA.Vc(DATA.xfin, DATA.L);
+    Vc = ENGfnc.Vc(ENG.xfin, ENG.L);
 end
 
 %__________________________________________________________ propellant data
 Pcbar = Pc / 1e5;
-gamma = DATA.g(Pcbar);
-if Pcbar > max(DATA.Pbar) * 2
+gamma = ENGfnc.g(Pcbar);
+if Pcbar > max(ENG.Pbar) * 2
     warning('Pressure is over double the maximum tabulated pressure')
     fprintf('Pc [bar] at the moment: %f bar\n', Pcbar)
-    fprintf('Maximum tabulated pressure: %f bar\n', max(DATA.Pbar))
+    fprintf('Maximum tabulated pressure: %f bar\n', max(ENG.Pbar))
     warning('Warning! There is also the possibility that this is a rejected step by the ode() function')
 end
 
@@ -33,21 +33,21 @@ PR = min(101325 / Pc, 0.999);
 choked = 1/PR > ( (gamma + 1)/2 )^(gamma/(gamma-1));
 
 if choked
-    cstar = DATA.cs(Pcbar);
-    mdot  = Pc * DATA.At / cstar;
+    cstar = ENGfnc.cs(Pcbar);
+    mdot  = Pc * ENG.At / cstar;
 else
-    R = DATA.R(Pcbar);
-    T = DATA.Tc(Pcbar);
+    R = ENGfnc.R(Pcbar);
+    T = ENGfnc.Tc(Pcbar);
     
-    mdot = DATA.At * Pc * sqrt(gamma/(R*T)) * ...
+    mdot = ENG.At * Pc * sqrt(gamma/(R*T)) * ...
            PR^(1/gamma) * ...
            sqrt(2/(gamma-1) * (1 - PR^((gamma-1)/gamma)));
 end
 
 
 
-mdot_gen = DATA.rhop * rb * Ab;
-Rt = DATA.R(Pcbar) * DATA.Tc(Pcbar);
+mdot_gen = ENG.rhop * rb * Ab;
+Rt = ENGfnc.R(Pcbar) * ENGfnc.Tc(Pcbar);
 
 f = zeros(3, 1);
 
--- SRE_dynamics/main.m
+++ SRE_dynamics/main.m
@@ -13,35 +13,41 @@ ENG.propl = load(FILENAMES.propl_data);
 ENG.grain = load(FILENAMES.grain_data);                                    % already in SI unities
 
 
-ENG.Ab   = @(x)    interp1(ENG.grain(:, 3), ENG.grain(:, 1), x);
-ENG.Vc   = @(x, L) interp1(ENG.grain(:, 3), ENG.grain(:, 2), x).*L;
+ENGfnc.Ab   = @(x)    interp1(ENG.grain(:, 3), ENG.grain(:, 1), x);
+ENGfnc.Vc   = @(x, L) interp1(ENG.grain(:, 3), ENG.grain(:, 2), x).*L;
 ENG.xfin = ENG.grain(end, 3);
 ENG.DCC  = ENG.grain(1, 4);
 
 ENG.Pbar  = ENG.propl(:, 1);                                              % bar
-ENG.Tc  = @(P) interp1(ENG.Pbar, ENG.propl(:, 2), P);                    
-ENG.g   = @(P) interp1(ENG.Pbar, ENG.propl(:, 3), P);
-ENG.gs  = @(P) interp1(ENG.Pbar, ENG.propl(:, 4), P);
-ENG.R   = @(P) interp1(ENG.Pbar, ENG.propl(:, 5), P);
-ENG.cs  = @(P) interp1(ENG.Pbar, ENG.propl(:, 6), P);
-ENG.rho = @(P) interp1(ENG.Pbar, ENG.propl(:, 7), P);
+ENGfnc.Tc  = @(P) interp1(ENG.Pbar, ENG.propl(:, 2), P);                    
+ENGfnc.g   = @(P) interp1(ENG.Pbar, ENG.propl(:, 3), P);
+ENGfnc.gs  = @(P) interp1(ENG.Pbar, ENG.propl(:, 4), P);
+ENGfnc.R   = @(P) interp1(ENG.Pbar, ENG.propl(:, 5), P);
+ENGfnc.cs  = @(P) interp1(ENG.Pbar, ENG.propl(:, 6), P);
+ENGfnc.rho = @(P) interp1(ENG.Pbar, ENG.propl(:, 7), P);
 
 ENG.L      = 1;                                                              % length of the CC (m)
 ENG.Vc_dry = ENG.L * (ENG.DCC^2/4 * pi);
 ENG.rhop   = 1750;                                                           % Kg/m3
 ENG.At     = 0.05;                                                            % m2
-ENG.mp     = ENG.rhop * (ENG.Vc_dry - ENG.Vc(ENG.grain(1, 3), ENG.L));
+ENG.mp     = ENG.rhop * (ENG.Vc_dry - ENGfnc.Vc(ENG.grain(1, 3), ENG.L));
 
 ENG.a    = 7e-5;                                                           % m/s/Pa
 ENG.n    = 0.4;
 
+%__________________________________________________ Simulink bus generation
+Simulink.Bus.createObject(ENG);
+ENGbus = slBus1;
+clear slBus1
 
+%_______________________________________________________ initial conditions
+y0 = [101325; ENG.grain(1, 3); ENG.mp];
 
 %% integration testing
 clc
-y0 = [101325; ENG.grain(1, 3); ENG.mp];
 
-[t, y] = ode45(@(t, y) internal_balistics(t, y, ENG), [0, 5], y0);
+
+[t, y] = ode45(@(t, y) internal_balistics(t, y, ENG, ENGfnc), [0, 5], y0);
 
 figure('Color',[1, 1, 1])
 plot(t, y(:, 1) / 1e5)
