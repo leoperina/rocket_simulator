4a1453dd99fd994d7740d4812c93663af72a4d02
leoperina <leonardoperina2003@gmail.com>
2026-04-07 22:10:42 +0200

    renamed DATA to ENG

--- SRE_dynamics/main.m
+++ SRE_dynamics/main.m
@@ -9,39 +9,39 @@ FILENAMES.grain_data = "meshNpropellant/grain_mesh/star_data_graphs.txt";
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %__________________________________ transforming data into useful functions
-DATA.propl = load(FILENAMES.propl_data);
-DATA.grain = load(FILENAMES.grain_data);                                    % already in SI unities
+ENG.propl = load(FILENAMES.propl_data);
+ENG.grain = load(FILENAMES.grain_data);                                    % already in SI unities
 
 
-DATA.Ab   = @(x)    interp1(DATA.grain(:, 3), DATA.grain(:, 1), x);
-DATA.Vc   = @(x, L) interp1(DATA.grain(:, 3), DATA.grain(:, 2), x).*L;
-DATA.xfin = DATA.grain(end, 3);
-DATA.DCC  = DATA.grain(1, 4);
+ENG.Ab   = @(x)    interp1(ENG.grain(:, 3), ENG.grain(:, 1), x);
+ENG.Vc   = @(x, L) interp1(ENG.grain(:, 3), ENG.grain(:, 2), x).*L;
+ENG.xfin = ENG.grain(end, 3);
+ENG.DCC  = ENG.grain(1, 4);
 
-DATA.Pbar  = DATA.propl(:, 1);                                              % bar
-DATA.Tc  = @(P) interp1(DATA.Pbar, DATA.propl(:, 2), P);                    
-DATA.g   = @(P) interp1(DATA.Pbar, DATA.propl(:, 3), P);
-DATA.gs  = @(P) interp1(DATA.Pbar, DATA.propl(:, 4), P);
-DATA.R   = @(P) interp1(DATA.Pbar, DATA.propl(:, 5), P);
-DATA.cs  = @(P) interp1(DATA.Pbar, DATA.propl(:, 6), P);
-DATA.rho = @(P) interp1(DATA.Pbar, DATA.propl(:, 7), P);
+ENG.Pbar  = ENG.propl(:, 1);                                              % bar
+ENG.Tc  = @(P) interp1(ENG.Pbar, ENG.propl(:, 2), P);                    
+ENG.g   = @(P) interp1(ENG.Pbar, ENG.propl(:, 3), P);
+ENG.gs  = @(P) interp1(ENG.Pbar, ENG.propl(:, 4), P);
+ENG.R   = @(P) interp1(ENG.Pbar, ENG.propl(:, 5), P);
+ENG.cs  = @(P) interp1(ENG.Pbar, ENG.propl(:, 6), P);
+ENG.rho = @(P) interp1(ENG.Pbar, ENG.propl(:, 7), P);
 
-DATA.L      = 1;                                                              % length of the CC (m)
-DATA.Vc_dry = DATA.L * (DATA.DCC^2/4 * pi);
-DATA.rhop   = 1750;                                                           % Kg/m3
-DATA.At     = 0.05;                                                            % m2
-DATA.mp     = DATA.rhop * (DATA.Vc_dry - DATA.Vc(DATA.grain(1, 3), DATA.L));
+ENG.L      = 1;                                                              % length of the CC (m)
+ENG.Vc_dry = ENG.L * (ENG.DCC^2/4 * pi);
+ENG.rhop   = 1750;                                                           % Kg/m3
+ENG.At     = 0.05;                                                            % m2
+ENG.mp     = ENG.rhop * (ENG.Vc_dry - ENG.Vc(ENG.grain(1, 3), ENG.L));
 
-DATA.a    = 7e-5;                                                           % m/s/Pa
-DATA.n    = 0.4;
+ENG.a    = 7e-5;                                                           % m/s/Pa
+ENG.n    = 0.4;
 
 
 
 %% integration testing
 clc
-y0 = [101325; DATA.grain(1, 3); DATA.mp];
+y0 = [101325; ENG.grain(1, 3); ENG.mp];
 
-[t, y] = ode45(@(t, y) internal_balistics(t, y, DATA), [0, 5], y0);
+[t, y] = ode45(@(t, y) internal_balistics(t, y, ENG), [0, 5], y0);
 
 figure('Color',[1, 1, 1])
 plot(t, y(:, 1) / 1e5)
