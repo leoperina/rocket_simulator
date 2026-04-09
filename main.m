c32c8facbe3a724702c30077da181b665f4e66a3
leoperina <leonardoperina2003@gmail.com>
2026-04-09 18:14:56 +0200

    bug fixing here and there

--- main.m
+++ main.m
@@ -3,63 +3,6 @@ close all
 clc
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ROCKET DYNAMICS
-
-AMB.g0     = 9.8065;                                                          % m/s2
-AMB.gamma  = 1.4;                                                            
-AMB.T0     = 288.15;                                                          % K
-AMB.R      = 8314 / 29;                                                       % J/KgK
-AMB.M      = sqrt(AMB.gamma*AMB.R*AMB.T0);
-AMB.lambda = -0.0065;
-AMBfnc.rho    = @(h) ...
-    1.225*(1 + AMB.lambda*h/AMB.T0).^(-AMB.g0/(AMB.R*AMB.lambda));
-AMB.dT     = -0.0065;
-AMB.P0     = 101325;
-
-
-% this will be rendered useless in Simulink
-MSL.Isp   = 300;                                                             % engine Isp, in [s]
-MSL.mp    = 0.5;                                                             % propellent weight (at startup)
-MSL.mdot  = -MSL.mp / 5;                                                     % mass flow out of the nozzle (negative since it's exiting the rocket)
-MSL.tb    = MSL.mp / abs(MSL.mdot);                                          % burning time [s] (after this time, thrust goes to zero)
-MSL.T     = MSL.Isp*abs(MSL.mdot)*AMB.g0;                                    % engine thrust, assumed constant [N]
-
-
-MSL.ms    = 500;                                                             % empty weight [Kg]
-MSL.m     = MSL.mp + MSL.ms;                                                 % starting mass
-MSL.l     = 1;                                                               % rocket length
-MSL.diam  = 0.12;                                                            % body diameter
-MSL.lc    = 0.45;                                                            % control arm
-MSL.la    = 0.20;                                                            % aerodinamic arm
-MSL.J     = MSL.m*MSL.l^2/12;                                                % moment of inertia
-MSL.clt   = 3.7;                                                             % cl_derivative [cl/rad]
-MSL.clts  = -2.4;                                                            % cl_der after stalling [cl/rad]
-MSL.thlim = deg2rad([-45, -20, 20, 45]);                                     % angle of attack limits. Over those values the rocket is in a stall and the simulation stops having physical sense
-MSL.thc   = 0.1;                                                             % viscous damping
-MSLfnc.cl    = @(th) ...
-    (th > MSL.thlim(2) & th <= MSL.thlim(3)) .* MSL.clt.*th + ...
-    (th > MSL.thlim(3) & th <= MSL.thlim(4)) .* ...
-    (MSL.clts.*(th - MSL.thlim(3)) + MSL.clt.*MSL.thlim(3)) + ...
-    (th >= MSL.thlim(1) & th < MSL.thlim(2)) .* ...
-    (MSL.clts.*(th - MSL.thlim(2)) + MSL.clt.*MSL.thlim(2));
-MSL.cd0   = 0.01;
-MSL.k     = 0.01;
-MSLfnc.cd    = @(th) MSL.cd0 + MSL.k*MSLfnc.cl(th).^2;
-MSL.y0_sim = [0; 0; 0; 0.1; 0; 0];
-
-%__________________________________________________ Simulink bus generation
-Simulink.Bus.createObject(MSL);
-MSLbus = slBus1;
-clear slBus1
-Simulink.Bus.createObject(AMB);
-AMBbus = slBus1;
-clear slBus1
-
-clearvars AMBfnc MSLfnc;
-
-%__________________________________________________________________________
-
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ENGINE MODEL
 
 FILENAMES.propl_data = "SRE_dynamics/meshNpropellant/propellant/totrash.txt";
@@ -108,3 +51,60 @@ disp('data loading complete, ready to pass in Simulink')
 disp('Filenames')
 disp(FILENAMES)
 clear FILENAMES;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ROCKET DYNAMICS
+
+AMB.g0     = 9.8065;                                                          % m/s2
+AMB.gamma  = 1.4;                                                            
+AMB.T0     = 288.15;                                                          % K
+AMB.R      = 8314 / 29;                                                       % J/KgK
+AMB.M      = sqrt(AMB.gamma*AMB.R*AMB.T0);
+AMB.lambda = -0.0065;
+AMBfnc.rho    = @(h) ...
+    1.225*(1 + AMB.lambda*h/AMB.T0).^(-AMB.g0/(AMB.R*AMB.lambda));
+AMB.dT     = -0.0065;
+AMB.P0     = 101325;
+
+
+% this will be rendered useless in Simulink
+% MSL.Isp   = 300;                                                             % engine Isp, in [s]
+% MSL.mp    = 0.5;                                                             % propellent weight (at startup)
+% MSL.mdot  = -MSL.mp / 5;                                                     % mass flow out of the nozzle (negative since it's exiting the rocket)
+% MSL.tb    = MSL.mp / abs(MSL.mdot);                                          % burning time [s] (after this time, thrust goes to zero)
+% MSL.T     = MSL.Isp*abs(MSL.mdot)*AMB.g0;                                    % engine thrust, assumed constant [N]
+
+
+MSL.ms    = 500;                                                             % empty weight [Kg]
+MSL.m     = ENG.mp + MSL.ms;                                                 % starting mass
+MSL.l     = 1;                                                               % rocket length
+MSL.diam  = 0.12;                                                            % body diameter
+MSL.lc    = 0.45;                                                            % control arm
+MSL.la    = 0.20;                                                            % aerodinamic arm
+MSL.J     = MSL.m*MSL.l^2/12;                                                % moment of inertia
+MSL.clt   = 3.7;                                                             % cl_derivative [cl/rad]
+MSL.clts  = -2.4;                                                            % cl_der after stalling [cl/rad]
+MSL.thlim = deg2rad([-45, -20, 20, 45]);                                     % angle of attack limits. Over those values the rocket is in a stall and the simulation stops having physical sense
+MSL.thc   = 0.1;                                                             % viscous damping
+MSLfnc.cl    = @(th) ...
+    (th > MSL.thlim(2) & th <= MSL.thlim(3)) .* MSL.clt.*th + ...
+    (th > MSL.thlim(3) & th <= MSL.thlim(4)) .* ...
+    (MSL.clts.*(th - MSL.thlim(3)) + MSL.clt.*MSL.thlim(3)) + ...
+    (th >= MSL.thlim(1) & th < MSL.thlim(2)) .* ...
+    (MSL.clts.*(th - MSL.thlim(2)) + MSL.clt.*MSL.thlim(2));
+MSL.cd0   = 0.01;
+MSL.k     = 0.01;
+MSLfnc.cd    = @(th) MSL.cd0 + MSL.k*MSLfnc.cl(th).^2;
+MSL.y0_sim = [0; 0; 0; 0.1; 0; 0];
+
+%__________________________________________________ Simulink bus generation
+Simulink.Bus.createObject(MSL);
+MSLbus = slBus1;
+clear slBus1
+Simulink.Bus.createObject(AMB);
+AMBbus = slBus1;
+clear slBus1
+
+clearvars AMBfnc MSLfnc;
+
+%__________________________________________________________________________
